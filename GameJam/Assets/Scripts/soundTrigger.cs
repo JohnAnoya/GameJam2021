1685e5a21121d1c5a15c808eaa894a9fbe11c978
CharlieCharlie79 <carloswork067@hotmail.com>
2021-01-30 18:29:43 -0500

    Fire

---
+++ GameJam/Assets/Scripts/soundTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundTrigger : MonoBehaviour
+{
+    public AudioSource playSound;
+
+    void OnTriggerEnter(Collider other)
+    {
+        playSound.Play();
+    }
+}
+
+
