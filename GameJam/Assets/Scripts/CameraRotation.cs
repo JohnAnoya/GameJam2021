89722a2f8f0227403ec1ae2d4802064a852da5d1
CrescentLua <45102273+CrescentLua@users.noreply.github.com>
2021-01-29 19:36:17 -0500

    Camera Rotation

---
+++ GameJam/Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    public Transform Player; 
+    float MouseSensitivity = 150.0f;
+    float XRotate = 0.0f;
+
+     void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseXAxis = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+        float mouseYAxis = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+
+        XRotate -= mouseYAxis;
+        XRotate = Mathf.Clamp(XRotate, -90.0f, 90.0f);
+
+        transform.localRotation = Quaternion.Euler(XRotate, 0.0f, 0.0f);
+        Player.Rotate(Vector3.up * mouseXAxis);
+    }
+}
--- GameJam/Assets/Scripts/Player.cs
+++ GameJam/Assets/Scripts/Player.cs
@@ -13,6 +13,6 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }
