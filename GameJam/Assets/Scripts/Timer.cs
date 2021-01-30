52f9a5157d90c760c104ef12cb04592e88d947be
tylercaldwell27 <44902413+tylercaldwell27@users.noreply.github.com>
2021-01-29 21:36:06 -0500

    Timer
    Code of the timer

---
+++ GameJam/Assets/Scripts/Timer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Timer : MonoBehaviour
+{
+    public float Seconds = 0;
+    public float Minutes = 1;
+    public Text timeDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeDisplay.text = Minutes.ToString() + "." + Seconds.ToString();
+        if (Seconds <= 0)
+        {
+            Seconds = 59;
+            if (Minutes >= 1)
+            {
+                Minutes--;
+            }
+            else
+            {
+                Minutes = 0;
+                Seconds = 0;
+
+            }
+        }
+        else
+        {
+            Seconds -= Time.deltaTime;
+        }
+
+    }
+}
