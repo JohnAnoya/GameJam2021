c3ec6aef4a5c31c9bb649b185da26b4b4595f9fc
CrescentLua <45102273+CrescentLua@users.noreply.github.com>
2021-01-30 10:26:00 -0500

    Moved Interaction System
    moved the interaction system from the player script into its own script

---
+++ GameJam/Assets/Scripts/InteractionSystem.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class InteractionSystem : MonoBehaviour
+{
+    List<string> Items = new List<string>();
+    List<string> Interactables = new List<string>();
+
+    public GameObject InteractionPopUp;
+    GameObject tempPopup;
+    bool showingPopup = false;
+
+    bool firePlace = true;
+    public ParticleSystem fireplaceEmitter;
+
+    [SerializeField]
+    Image note1;
+    bool showingNote = false;
+    //[SerializeField]
+    //Image hintBook;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Items.Add("Key");
+        Interactables.Add("FireplaceSwitch");
+        Interactables.Add("Note_1");
+        //Interactables.Add("Book_Open");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckForInteractions(); 
+    }
+
+    void CheckForInteractions()
+    {
+        var PlayerScript = gameObject.GetComponent<Player>(); 
+
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1.0f))
+        {
+            Debug.DrawRay(Camera.main.transform.position, ray.direction, Color.green);
+            if (hit.transform.name == "Key")
+            {
+                if (!showingPopup && !firePlace)
+                {
+                    showingPopup = true;
+                    tempPopup = Instantiate(InteractionPopUp, new Vector3(hit.transform.position.x, hit.transform.position.y + 0.25f, hit.transform.position.z - 0.4f), Quaternion.identity);
+                    tempPopup.GetComponentInChildren<TMP_Text>().SetText("Pick up Key");
+                }
+
+                if (!PlayerScript.CheckInventory(hit.transform.name) && Input.GetMouseButtonDown(0) && !firePlace)
+                {
+                    Items.Remove(hit.transform.name);
+                    PlayerScript.AddToInventory(hit.transform.name);
+                    Destroy(hit.transform.gameObject);
+                    Destroy(tempPopup);
+                    showingPopup = false;
+                    Debug.Log("Key picked up");
+                }
+            }
+
+            else if (hit.transform.name == "FireplaceSwitch")
+            {
+                if (!showingPopup)
+                {
+                    showingPopup = true;
+                    tempPopup = Instantiate(InteractionPopUp, new Vector3(hit.transform.position.x + 0.5f, hit.transform.position.y + 0.9f, hit.transform.position.z), Quaternion.identity);
+
+                    if (firePlace)
+                    {
+                        tempPopup.GetComponentInChildren<TMP_Text>().SetText("Turn off Fireplace");
+                    }
+
+                    else
+                    {
+                        tempPopup.GetComponentInChildren<TMP_Text>().SetText("Turn on Fireplace");
+                    }
+                }
+
+                if (Interactables.Contains(hit.transform.name) && Input.GetMouseButtonDown(0))
+                {
+                    if (firePlace)
+                    {
+                        tempPopup.GetComponentInChildren<TMP_Text>().SetText("Turn on Fireplace");
+                        Debug.Log("Turned off Fireplace");
+
+                        firePlace = false;
+                        fireplaceEmitter.Stop();
+                    }
+
+                    else
+                    {
+                        tempPopup.GetComponentInChildren<TMP_Text>().SetText("Turn off Fireplace");
+                        Debug.Log("Turned on Fireplace");
+                        firePlace = true;
+                        fireplaceEmitter.Play();
+                    }
+
+                }
+            }
+
+            else if (hit.transform.name == "Note_1")
+            {
+                if (!showingPopup)
+                {
+                    showingPopup = true;
+                    tempPopup = Instantiate(InteractionPopUp, new Vector3(hit.transform.position.x, hit.transform.position.y + 1.0f, hit.transform.position.z - 0.4f), Quaternion.identity);
+                    tempPopup.GetComponentInChildren<TMP_Text>().SetText("Pick up note");
+                }
+
+                if (Interactables.Contains(hit.transform.name) && Input.GetMouseButtonDown(0) && !showingNote)
+                {
+                    note1.enabled = true;
+                    showingNote = true;
+                }
+                else if (showingNote && Input.GetMouseButtonDown(0))
+                {
+                    note1.enabled = false;
+                    showingNote = false;
+                }
+            }
+
+        }
+
+        else if (showingPopup)
+        {
+            showingPopup = false;
+            Destroy(tempPopup);
+        }
+    }
+}
--- GameJam/Assets/Scripts/Player.cs
+++ GameJam/Assets/Scripts/Player.cs
@@ -6,8 +6,6 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    List<string> Items = new List<string>();
-    List<string> Interactables = new List<string>();
     List<string> Inventory = new List<string>();
     
     public Camera camera; 
@@ -17,27 +15,11 @@ public class Player : MonoBehaviour
     float gravity = -9.81f;
     float thickness = 1.0f;
 
-    public GameObject InteractionPopUp;
-    GameObject tempPopup; 
-    bool showingPopup = false;
-
-    bool firePlace = true;
-    public ParticleSystem fireplaceEmitter;
-
-    [SerializeField]
-    Image note1;
-    bool showingNote = false;
-    //[SerializeField]
-    //Image hintBook;
-
     // Start is called before the first frame update
     void Start()
     {
         characterController = gameObject.GetComponent<CharacterController>();
-        Items.Add("Key");
-        Interactables.Add("FireplaceSwitch");
-        Interactables.Add("Note_1");
-        //Interactables.Add("Book_Open");
+
     }
 
     // Update is called once per frame
@@ -49,101 +31,27 @@ public class Player : MonoBehaviour
         Vector3 move = transform.right * XAxis + transform.forward * ZAxis;
         characterController.Move(move * Time.deltaTime * WalkSpeed);
   
-        Ray ray = camera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit, 1.0f))
-        {
-            Debug.DrawRay(camera.transform.position, ray.direction, Color.green);
-            if (hit.transform.name == "Key")
-            {
-                if (!showingPopup && !firePlace)
-                {
-                    showingPopup = true;
-                    tempPopup = Instantiate(InteractionPopUp, new Vector3(hit.transform.position.x, hit.transform.position.y + 0.25f, hit.transform.position.z - 0.4f), Quaternion.identity);
-                    tempPopup.GetComponentInChildren<TMP_Text>().SetText("Pick up Key");
-                }
-
-                if (!Inventory.Contains(hit.transform.name) && Input.GetMouseButtonDown(0) && !firePlace) {
-                    Items.Remove(hit.transform.name);
-                    Inventory.Add(hit.transform.name);
-                    Destroy(hit.transform.gameObject);
-                    Destroy(tempPopup);
-                    showingPopup = false;
-                    Debug.Log("Key picked up");
-                }
-            }
-
-            else if (hit.transform.name == "FireplaceSwitch")
-            {
-                if (!showingPopup)
-                {
-                    showingPopup = true;
-                    tempPopup = Instantiate(InteractionPopUp, new Vector3(hit.transform.position.x + 0.5f, hit.transform.position.y + 0.9f, hit.transform.position.z), Quaternion.identity);
-
-                    if (firePlace)
-                    {
-                        tempPopup.GetComponentInChildren<TMP_Text>().SetText("Turn off Fireplace");
-                    }
-
-                    else
-                    {
-                        tempPopup.GetComponentInChildren<TMP_Text>().SetText("Turn on Fireplace");
-                    }
-                }
-
-                if (Interactables.Contains(hit.transform.name) && Input.GetMouseButtonDown(0))
-                {
-                    if (firePlace)
-                    {
-                        tempPopup.GetComponentInChildren<TMP_Text>().SetText("Turn on Fireplace");
-                        Debug.Log("Turned off Fireplace");
-
-                        firePlace = false;
-                        fireplaceEmitter.Stop();
-                    }
-
-                    else
-                    {
-                        tempPopup.GetComponentInChildren<TMP_Text>().SetText("Turn off Fireplace");
-                        Debug.Log("Turned on Fireplace");
-                        firePlace = true;
-                        fireplaceEmitter.Play();
-                    }
-
-                }
-            }
-
-            else if (hit.transform.name == "Note_1")
-            {
-                if (!showingPopup)
-                {
-                    showingPopup = true;
-                    tempPopup = Instantiate(InteractionPopUp, new Vector3(hit.transform.position.x, hit.transform.position.y + 1.0f, hit.transform.position.z - 0.4f), Quaternion.identity);
-                    tempPopup.GetComponentInChildren<TMP_Text>().SetText("Pick up note");
-                }
-            
-                if (Interactables.Contains(hit.transform.name) && Input.GetMouseButtonDown(0) && !showingNote)
-                {
-                    note1.enabled = true;
-                    showingNote = true;
-                }
-                else if (showingNote && Input.GetMouseButtonDown(0))
-                {
-                    note1.enabled = false;
-                    showingNote = false;
-                }
-            }
-           
-        }
-
-        else if (showingPopup)
-        {
-            showingPopup = false;
-            Destroy(tempPopup);
-        }
+       
 
         playerVel.y += gravity * Time.deltaTime;
         characterController.Move(playerVel * Time.deltaTime);
+    }
+
+   public void AddToInventory(string item_)
+    {
+        Inventory.Add(item_);
+    }
+
+    public bool CheckInventory(string item_)
+    {
+        if (Inventory.Contains(item_))
+        {
+            return true; 
+        }
+
+        else
+        {
+            return false;
+        }
     }
 }
