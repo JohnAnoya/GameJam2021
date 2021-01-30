651703128ec7b9c789a5438c45570b34d5689318
CrescentLua <45102273+CrescentLua@users.noreply.github.com>
2021-01-29 19:55:35 -0500

    Player Movement

--- GameJam/Assets/Scripts/Player.cs
+++ GameJam/Assets/Scripts/Player.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    CharacterController characterController; 
+    Vector3 playerVel;
+    float WalkSpeed = 8.0f;
+    float gravity = -9.81f;
+
     // Start is called before the first frame update
     void Start()
     {
+        characterController = gameObject.GetComponent<CharacterController>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        float XAxis = Input.GetAxis("Horizontal");
+        float ZAxis = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * XAxis + transform.forward * ZAxis;
+        characterController.Move(move * Time.deltaTime * WalkSpeed);
+
+
+        //playerVel.y += gravity * Time.deltaTime;
+        //characterController.Move(playerVel * Time.deltaTime);
     }
 }
