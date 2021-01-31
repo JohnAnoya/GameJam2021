2ddeb10985092e6b6b10448187295cdf3f90f25b
tylercaldwell27 <44902413+tylercaldwell27@users.noreply.github.com>
2021-01-30 20:47:14 -0500

    Update Timer.cs

--- GameJam/Assets/Scripts/Timer.cs
+++ GameJam/Assets/Scripts/Timer.cs
@@ -7,8 +7,8 @@ using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    static public float Seconds = 10;
-    static public float Minutes = 0;
+    static public float Seconds = 0;
+    static public float Minutes = 10;
     public TMP_Text timeDisplay;
 
     // Start is called before the first frame update
