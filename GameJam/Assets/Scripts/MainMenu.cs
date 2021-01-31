7e5f8b120bf3039ac50c32599ebce8435c793418
CrescentLua <45102273+CrescentLua@users.noreply.github.com>
2021-01-31 12:26:40 -0500

    Small changes made to each scene + winning scene added

--- GameJam/Assets/Scripts/MainMenu.cs
+++ GameJam/Assets/Scripts/MainMenu.cs
@@ -6,9 +6,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
     public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void GoBackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 
     public void Exit ()
